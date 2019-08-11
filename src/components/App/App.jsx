879e40043e6e099838ac6205b661ab048f66b28d
TyraelS <oshynkarenko1503@gmail.com>
2019-08-11 18:38:36 +0300

    returned App.jsx to its former glory

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { theme, GlobalStyle } from '../../themes';
-import MasterPage from '../pages/MasterPage';
+import { theme } from '../../themes';
 
 const displayName = 'App';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <>
-        <Route
-          path="/"
-          exact
-          component={() => (
-            <>
-              <MasterPage pageTitle="SomeText" isLogged={false}>
-                <span>Some shit</span>
-              </MasterPage>
-              <GlobalStyle />
-            </>
-          )}
-        />
-      </>
+      <Route path="/" exact />
     </ThemeProvider>
   );
 }
